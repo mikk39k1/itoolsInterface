2e98827599161fe2b4897d29d314d3ee4af352e8
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-06-13 12:11:20 +0200

    Update build.sh

--- jar/build.sh
+++ jar/build.sh
@@ -4,4 +4,4 @@
 cd /itoolsInterface/target
 
 # Run the build command (example: using Maven)
-mvn -f /home/fomadmin/itoolsInterface/pom.xml clean package
+mvn -f /home/fomadmin/itoolsInterface clean package
