ae964c87ccf99dff0874654533dbf289fb538420
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-06-13 11:20:46 +0200

    Update build.sh

--- jar/build.sh
+++ jar/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to the project directory
-cd /home/fomadmin/itoolsInterface/jar/app.jar
+cd /home/fomadmin/itoolsInterface
 
 # Run the build command (example: using Maven)
 mvn clean package
