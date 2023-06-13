32dc8275acf029de73382da3d9b9ce00586970de
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-06-13 11:44:36 +0200

    Update build.sh

--- jar/build.sh
+++ jar/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Grant read permissions to pom.xml file
-chmod +r /home/fomadmin/itoolsInterface/pom.xml
+chmod +r /home/runner/work/itoolsInterface/itoolsInterface/pom.xml
 
 # Change directory to project directory
-cd /home/fomadmin/itoolsInterface
+cd /home/runner/work/itoolsInterface/itoolsInterface
 
 # Run Maven build
 mvn clean install
 
-# Move the generated JAR file to the jar directory a
-mv /home/fomadmin/itoolsInterface/target/app.jar /home/fomadmin/itoolsInterface/jar/
+# Move the generated JAR file to the jar directory
+mv /home/runner/work/itoolsInterface/itoolsInterface/target/app.jar /home/runner/work/itoolsInterface/itoolsInterface/jar/
