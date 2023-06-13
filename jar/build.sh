05eeddf823179c932f692e7b159f83ec5f637fd9
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-06-13 11:45:49 +0200

    Update build old script.sh

--- jar/build.sh
+++ jar/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Grant read permissions to pom.xml file
-chmod +r /home/runner/work/itoolsInterface/itoolsInterface/pom.xml
+# Go to the project directory
+cd /path/to/project
 
-# Change directory to project directory
-cd /home/runner/work/itoolsInterface/itoolsInterface
-
-# Run Maven build
-mvn clean install
-
-# Move the generated JAR file to the jar directory
-mv /home/runner/work/itoolsInterface/itoolsInterface/target/app.jar /home/runner/work/itoolsInterface/itoolsInterface/jar/
+# Run the build command (example: using Maven)
+mvn clean package
