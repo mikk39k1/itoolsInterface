7bcb2f2e12a03aca90d439e93a8443e4579896de
mikk39k1 <114911882+mikk39k1@users.noreply.github.com>
2023-07-17 23:57:59 -1200

    Nu med timestmap i data og some schedule ved midnat til at fjerne data i liste

--- src/main/java/com/example/itoolsinterface/controller/TemporaryAccessController.java
+++ src/main/java/com/example/itoolsinterface/controller/TemporaryAccessController.java
@@ -1,8 +1,11 @@
 package com.example.itoolsinterface.controller;
 
 import com.example.itoolsinterface.model.TemporaryAccessData;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +14,19 @@ public class TemporaryAccessController {
 
     private List<TemporaryAccessData> temporaryAccessDataList = new ArrayList<>();
 
+    @Scheduled(cron = "0 0 0 * * *")
+    public void clearDataFromListAtMidnight() {
+        LocalDateTime now = LocalDateTime.now();
+        temporaryAccessDataList.removeIf(data ->
+                data.getLocalDateTime().toLocalDate().isAfter(now.toLocalDate()));
+    }
+
+
     @PostMapping("/getTemporaryAccess")
     public String submitTemporaryAccess(@RequestParam("hourAmount") int hourAmount,
-                                              @RequestParam("mailGroup") String mailGroup,
-                                              @RequestParam("initials") String initials) {
-        TemporaryAccessData temporaryAccessData = new TemporaryAccessData(hourAmount, mailGroup, initials);
+                                        @RequestParam("mailGroup") String mailGroup,
+                                        @RequestParam("initials") String initials) {
+        TemporaryAccessData temporaryAccessData = new TemporaryAccessData(hourAmount, mailGroup, initials, LocalDateTime.now());
         temporaryAccessDataList.add(temporaryAccessData);
         if (temporaryAccessDataList.size() >= 2){
             temporaryAccessDataList.remove(0);
--- src/main/java/com/example/itoolsinterface/model/TemporaryAccessData.java
+++ src/main/java/com/example/itoolsinterface/model/TemporaryAccessData.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Getter
 @Setter
@@ -11,10 +13,12 @@ public class TemporaryAccessData {
     private String initials;
     private int hourAmount;
     private String mailGroup;
+    private LocalDateTime localDateTime;
 
-    public TemporaryAccessData(int hourAmount, String mailGroup, String initials) {
+    public TemporaryAccessData(int hourAmount, String mailGroup, String initials, LocalDateTime localDateTime) {
         this.hourAmount = hourAmount;
         this.mailGroup = mailGroup;
         this.initials = initials;
+        this.localDateTime = localDateTime;
     }
 }
